c646c018c249213d8967c02f4e2f8c043d9eea76
devtescode <teslimagboola09@gmail.com>
2026-06-04 18:03:49 +0100

    update code

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -156,7 +156,7 @@ module.exports.initializePayment = async (req, res) => {
       {
         email,
         amount: amount * 100,
-        callback_url: "https://dunkacyce-backend.onrender.com/orders",
+        callback_url: "https://dunnkayce-navy.vercel.app/orders",
       },
       {
         headers: {
